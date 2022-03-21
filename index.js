e84f4565cf69daf321c86baad9dbfd522abaed73
Daniel Ezra <danielezraa@gmail.com>
2022-03-21 16:30:49 +0200

    uploading server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:312166390@34.79.81.80:5432/postgres')
+
+
+app.get('/', async (req, res) => {
+    res.send("hello world!")
+})
+
+app.get('/users', async (req, res) => {
+    await db.any('SELECT * from users')
+  .then((data) => {
+    res.send(JSON.stringify(data));
+  })
+  .catch((error) => {
+      res.send("error at express server, selecting data")
+    console.log('ERROR:', error)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`SeaData express server listening on port ${port}`)
+})
